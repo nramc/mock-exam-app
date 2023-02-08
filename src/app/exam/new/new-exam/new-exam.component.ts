9c77de35a8d4b58a1e756b49a7080636ba66d55a
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-02-08 19:06:41 +0100

    feat: Preview exam data.json file feature
     - mat table prototype added for new exam questions

--- src/app/exam/new/new-exam/new-exam.component.ts
+++ src/app/exam/new/new-exam/new-exam.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {NewExamService} from "../services/new-exam.service";
 import {Observable, of} from "rxjs";
+import {StepperOrientation} from "@angular/cdk/stepper";
+import {BreakpointObserver} from "@angular/cdk/layout";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-new-exam',
@@ -8,10 +11,17 @@ import {Observable, of} from "rxjs";
   styleUrls: ['./new-exam.component.scss']
 })
 export class NewExamComponent {
-  constructor(private newExamService: NewExamService) {
+  stepperOrientation: Observable<StepperOrientation>;
+
+  constructor(private newExamService: NewExamService,
+              private breakpointObserver: BreakpointObserver
+  ) {
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({matches}) => (matches ? 'horizontal' : 'vertical')));
   }
 
   hasAnyErrorInDetailsForm(): Observable<boolean> {
-    return of(! this.newExamService.hasValidExamDetails());
+    return of(!this.newExamService.hasValidExamDetails());
   }
 }
