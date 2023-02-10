8e7f606f910d966de1a8b2d83bc10ebf8a36a935
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-02-10 19:58:33 +0100

    feat: Preview exam data.json file feature
     - new exam summary added

---
+++ src/app/exam/new/new-exam-summary/new-exam-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-exam-summary',
+  templateUrl: './new-exam-summary.component.html',
+  styleUrls: ['./new-exam-summary.component.scss']
+})
+export class NewExamSummaryComponent {
+
+}
