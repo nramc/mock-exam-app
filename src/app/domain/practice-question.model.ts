02fb60a453b5252e30e5e6a711ba01f452288048
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-02-15 19:48:03 +0100

    feat: Add title field to question #52
     - title field added to question model
     - practice question component adopted to new question.title field

--- src/app/domain/practice-question.model.ts
+++ src/app/domain/practice-question.model.ts
@@ -3,6 +3,7 @@ import {PracticeOption} from "./practice-option.model";
 
 export interface PracticeQuestion {
   id: string,
+  title: string,
   description: string,
   hasMultipleAnswers: boolean
   options: PracticeOption[],
