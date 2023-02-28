af75e5e3801eab8d9f46c057dbe7098139444fc5
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2023-02-28 22:26:07 +0100

    feat: standalone tag name & tag list component created to align throughout project

---
+++ src/app/components/tagname.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-tag-name',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<span class="badge text-bg-primary me-1">#&nbsp;{{tagName}}</span>`,
+})
+export class TagNameComponent {
+  @Input()
+  tagName!: string;
+
+}
+
+@Component({
+  selector: 'app-tag-list',
+  standalone: true,
+  imports: [CommonModule, TagNameComponent],
+  template: `
+    <ng-container *ngFor="let tag of tags">
+      <app-tag-name [tagName]="tag"></app-tag-name>
+    </ng-container>`,
+})
+export class TagListComponent {
+  @Input()
+  tags!: string[];
+
+}
